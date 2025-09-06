ea0f5f22b881eb5d71a392d22b495a1272416955
WitchDevelops <d.n.wojewska@gmail.com>
2025-09-06 22:34:18 +0200

    feat: introduce deleting note functionality

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -2,13 +2,34 @@ import { Link } from "react-router";
 import { PenSquareIcon, TrashIcon } from "lucide-react";
 import type { Note } from "../types";
 import { formatDate } from "../lib/utils";
-export const NoteCard: React.FC<Note> = ({
+import { api } from "../lib/axios";
+import toast from "react-hot-toast";
+
+interface NoteCardProps extends Note {
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}
+export const NoteCard: React.FC<NoteCardProps> = ({
   _id,
   title,
   content,
   createdAt,
   updatedAt,
+  setNotes,
 }) => {
+  const handleDeleteNote = async (e: React.MouseEvent, id: string) => {
+    e.preventDefault(); // Prevent navigation
+    if (!window.confirm("Are you sure you want to delete this note?")) {
+      return;
+    }
+    try {
+      await api.delete(`/notes/${id}`);
+      toast.success("Note deleted successfully.");
+      setNotes((prev) => prev.filter((note) => note._id !== id));
+    } catch (error) {
+      console.error("Error deleting note:", error);
+      toast.error("An error occurred while deleting the note.");
+    }
+  };
   return (
     <Link
       to={`/note/${_id}`}
@@ -30,7 +51,12 @@ export const NoteCard: React.FC<Note> = ({
             <button className="btn btn-ghost btn-sm btn-circle hover:bg-primary/10 hover:text-primary">
               <PenSquareIcon className="size-5 text-base-content/70" />
             </button>
-            <button className="btn btn-ghost btn-sm btn-circle hover:bg-red-500/10 hover:text-error">
+            <button
+              className="btn btn-ghost btn-sm btn-circle hover:bg-red-500/10 hover:text-error"
+              onClick={(e) => {
+                handleDeleteNote(e, _id);
+              }}
+            >
               <TrashIcon className="size-5 text-base-content/70 hover:text-error" />
             </button>
           </div>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -44,7 +44,7 @@ export const HomePage = () => {
         {!loading && !isRateLimited && notes.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {notes.map((note) => (
-              <NoteCard key={note._id} {...note} />
+              <NoteCard key={note._id} {...note} setNotes={setNotes} />
             ))}
           </div>
         )}
