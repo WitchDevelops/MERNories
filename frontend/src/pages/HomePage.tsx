43a418c6352a424eb5715aedf26ffa5699d3f0bd
WitchDevelops <d.n.wojewska@gmail.com>
2025-09-06 18:16:29 +0200

    feat: install tailwind, daisyui, toaster

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,18 @@
-
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div data-theme="dracula">
+      <h1 className="text-3xl font-bold text-blue-600 bg-blue-200 p-4">
+        home page
+      </h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
