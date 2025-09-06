10a889b8a5084c8f963d6378376a42c7eabcf116
WitchDevelops <d.n.wojewska@gmail.com>
2025-09-06 17:43:15 +0200

    feat: add middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 console.log("Starting server...");
 console.log("Connecting to database...");
@@ -9,14 +10,16 @@ console.log("Connecting to database...");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
-connectDb();
+const PORT = process.env.PORT || 5001;
 
 //middleware
-app.use(express.json());
+app.use(express.json()); // to parse JSON request bodies
+app.use(rateLimiter); // apply rate limiting middleware
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
